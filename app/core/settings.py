3f24cdd04c876ccedcaf1e29573c60272de968d0
Anar Bairam <anar.bayram@yahoo.com>
2026-01-31 00:44:05 +0400

    feat: dockerized the app

--- app/core/settings.py
+++ app/core/settings.py
@@ -6,6 +6,10 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DEBUG: bool
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    MAIN_DB_NAME: str
+    TENANT_DB_NAME: str
     MAIN_DB_URL: PostgresDsn
     TENANT_DB_URL: PostgresDsn
     ACCESS_TOKEN_EXPIRE: int
